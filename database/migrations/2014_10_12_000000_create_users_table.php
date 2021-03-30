d1b14377fa9168c870cbbc74517d4442da82ed19
MASBRO <s2gotosingapore@gmail.com>
2021-03-30 13:02:36 +0700

    Goss

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('nama');
+            $table->string('name');
             $table->string('nik');
             $table->string('email')->unique();
             $table->string('password');
