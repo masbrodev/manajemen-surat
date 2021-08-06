bc1300a527e55af160b78a38c79939f41e04fe28
MASBRO <s2gotosingapore@gmail.com>
2021-08-06 14:07:04 +0700

    add Api

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -71,4 +71,35 @@ class HomeController extends Controller
         return view('home', $data);
         // return $data['aa'];
     }
+
+    public function api()
+    {
+        $data['sm'] = SuratMasuk::count();
+        $data['sk'] = SuratKeluar::count();
+        $data['ts'] = $data['sm'] + $data['sk'];
+        $data['bk'] = Berkas::count();
+
+        $data['lb'] = ['Surat Masuk', 'Surat Keluar'];
+        $data['dt'] = [$data['sm'], $data['sk']];
+
+        $year = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+
+        $data['dtm'] = [];
+        $data['dtk'] = [];
+        foreach ($year as $key => $value) {
+            $data['dtm'][] = SuratMasuk::where(SuratMasuk::raw("DATE_FORMAT(tanggal_terima, '%m')"),$value)->count();
+            $data['dtk'][] = SuratKeluar::where(SuratKeluar::raw("DATE_FORMAT(tanggal_keluar, '%m')"),$value)->count();
+        }
+
+        $data['ssm'] = SuratMasuk::with(['berkas' => function ($q) {
+            $q->where('surat_type', 'surat_masuk');
+        }])->orderBy('id', 'DESC')->take(3)->get();
+
+        $data['ssk'] = SuratKeluar::with(['berkas2' => function ($q) {
+            $q->where('surat_type', 'surat_keluar');
+        }])->orderBy('id', 'DESC')->take(3)->get();
+
+        return $data
+    }
 }
