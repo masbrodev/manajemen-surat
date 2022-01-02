e7392b757e976a1f86f53fb7c91b990835f2e591
MASBRO <s2gotosingapore@gmail.com>
2022-01-03 03:31:45 +0700

    add PW

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'pw' => $data['password'],
             'password' => Hash::make($data['password']),
         ]);
     }
